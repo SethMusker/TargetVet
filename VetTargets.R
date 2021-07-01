0bd34347f81bd96c8d788e3d8ada9ff6e6e0f954
Seth Musker <sethmusker@gmail.com>
2021-07-01 11:29:57 +0200

    added plotting (1D)

--- VetTargets.R
+++ VetTargets.R
@@ -14,7 +14,7 @@ filterTargets<-function(cov,target,filterParalogs,out){
             group_by(ID) %>%
             summarise(median_cov=median(coverage),
                         sd_cov=round(sd(coverage),1)) %>%
-            arrange(desc(median_cov)) %>%
+            arrange(dplyr::desc(median_cov)) %>%
             mutate(ID = fct_reorder(ID, dplyr::desc(median_cov)))
         incov_summary[[i]]$ord<-c(1:nrow(incov_summary[[i]]))
         myseg<-segmented(lm(median_cov~ord,data=incov_summary[[i]]),npsi=2)
@@ -26,10 +26,27 @@ filterTargets<-function(cov,target,filterParalogs,out){
         } else {
             mychosen[[i]]<-incov_summary[[i]] %>% dplyr::filter(median_cov >= breakpoint_low_mc, median_cov <= breakpoint_high_mc) %>% select(ID) %>% as.matrix() %>% as.vector()
         }
+        ## make plots
+        incov_summary[[i]]$segment_line<-predict(myseg)
+        incov_summary[[i]]$segment_line[incov_summary[[i]]$segment_line < 0]<-0
+        myplot<-ggplot(incov_summary[[i]])+
+            geom_col(aes(x=ID,y=median_cov,fill=sd_cov))+
+            scale_fill_viridis_c("Coverage SD")+
+            theme(axis.text.x  = element_text(angle=90,size=5.5))+
+            scale_y_continuous(n.breaks = 25)+
+            geom_hline(yintercept = breakpoint_high_mc,colour="red")+
+            geom_hline(yintercept = breakpoint_low_mc,colour="red")+
+            geom_line(aes(x=ord,y=segment_line),size=1,colour="green",alpha=0.8)+
+            ylim(c(0,max(incov_summary[[i]]$median_cov)))+
+            ylab("Median Coverage")
+        ggsave(paste0(i,".pdf"),plot=myplot,width=25,height=20,units="cm")
+
     }
     mychosen_intersect<-Reduce(intersect, mychosen)
+    mychosen_outersect<-incov_summary[[i]]$ID[! incov_summary[[i]]$ID %in% mychosen_intersect]
     cat("Filtering kept",length(mychosen_intersect),"out of",length(incov_summary[[i]]$ID),"targets")
     write.table(data.frame(ID=mychosen_intersect),paste0(out,".kept"),col.names=F,row.names=F,quote=F)
+    write.table(data.frame(ID=mychosen_outersect),paste0(out,".removed"),col.names=F,row.names=F,quote=F)
     myout_target<-readDNAStringSet(target)[mychosen_intersect]
     writeXStringSet(myout_target,out)
 }
--- map_WGS_to_targets.sh
+++ map_WGS_to_targets.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## this script maps your WGS paired-end reads to your target set and computes the coverage at each location
-## Dependencies: BWA and samtools 
+## Dependencies: BWA, samtools 
 
 while getopts F:R:T:n: option
 do
@@ -16,9 +16,9 @@ n) threads=${OPTARG};;
 esac
 done
 
-bwa index $TARGET
-bwa mem -t $threads $TARGET $READS1 $READS2 | samtools view -@ $threads -F 4 -q 20 -b | samtools sort > `basename $READS1 .fastq.gz`_to_${TARGET}.sorted.bam
-BAM=`basename $READS1 .fastq.gz`_to_${TARGET}.sorted.bam
-samtools index $BAM
+bwa index ${TARGET}
+bwa mem -t ${threads} ${TARGET} ${READS1} ${READS2} | samtools view -@ ${threads} -F 4 -q 20 -b | samtools sort > `basename ${READS1} .gz`_to_${TARGET}.sorted.bam
+BAM=`basename ${READS1} .gz`_to_${TARGET}.sorted.bam
+samtools index ${BAM}
 
-samtools depth -a $BAM > ${BAM}.coverage
+samtools depth -a ${BAM} > ${BAM}.coverage
