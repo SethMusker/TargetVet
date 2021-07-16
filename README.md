a8f47c8b2e158f3ad023a093599dd9bf0bb9a37e
Seth Musker <sethmusker@gmail.com>
2021-07-16 11:35:31 +0200

    subsumed TargetSupercontigs repo
    Various improvements to code.
    Improved Readme

--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-So you're doing a very important phylogenomics and you've designed or chosen your target set for your favorite organism. What next? Well, you've also got some genomic shotgun sequence data for some closely related species. Rather than moving forward with your sequencing project and hoping for decent recovery without too many nasty paralogs, it's possible to use that WGS data to make life for your future self a little easier, while also improving the efficiency of your project.
 
-This repo provides methods for using WGS data to vet (i.e. filter) your target set prior to bait design. 
+## TL;DR
+Evolutionary studies using target capture (Hyb-seq): Use avaliable WGS raw reads or a reference genome (draft genomes should work) to:
+ 1. identify putative **paralogs** and **missing genes** (both `VetTargets_WGS.R` and `VetTargets_genome.R`).
+ 2. identify genes with **huge intron(s)** (which are often present e.g. in many mammals [[(1)]] and should definitely NOT be assumed to be in linkage equilibrium). (Only `VetTargets_genome.R`).
+ 3. Nicely visualise the **genomic context** of your targets relative to your study group.
+ 4. Extract supercontigs for use as targets for phylogenetics of closely related species.
+ 5. ## `VetTargets_WGS.R` and `VetTargets_genome.R`
 
+So you're doing a very important phylogenomics and you've designed or chosen your target set for your favorite organism. What next? Well, you've also got some genomic shotgun sequence data and/or a reference genome for one or more closely related species. Rather than moving forward with your sequencing project and hoping for decent recovery without too many nasty paralogs or other surprises, it's possible to use that WGS data to make life for your future self a little easier while also improving the efficiency of your project.
+
+TargetVet provides methods for using WGS data and/or a reference genome to vet (i.e. filter) your target set before bait design. 
+
+In the event that you're already with target capture data in hand, you can still use TargetVet to help filter your assembled genes (e.g. to identify paralogs and guide their separate assembly).
+
+### Usage
+#### `VetTargets_genome.R`
+This takes a blast search of your targets to your genome and outputs the following:
+1. `<output_prefix>.IntronFlags.txt`: Per-target summary of introns/intergenic regions with logical flags for exceeding specified max intron length and percent of supercontig made up of introns, as well as the actual values.
+2. `<output_prefix>.IntronStats.txt`: Position and length of each intron on each target.
+3. `<output_prefix>.CoverageStats_AcrossChromosomes.txt`: Per-target summary of coverage across 'chromosomes' in the reference genome. Fields are `qseqid mean_paralogy_rate paralog_percent full_percent unique_percent missing_percent n_chroms`. This is informative about **paralogs**. You will be most interested in `mean_paralogy_rate`, which is the average number of segments from the genome covering each position in the target. Single-copy loci will have a value around 1, duplicates around 2, triplicates around 3, etc. This value only considers target segments with hits, so the minumim value is 1. To check for missing targets, look at `missing_percent`, which is the proportion of the target without any matches. Remember that if you set `--min_fragment_length` too high, `missing_percent` will be inflated. Keeping it at zero should be fine.
+```
+Rscript VetTargets_genome.R --blast_file blastn_targets_to_genome.withHeader.txt --min_fragment_length 0 --min_pident 70 --max_intron_length 1000 --max_intron_percent 50 --output_prefix targets2genome --min_display_intron 300
+```
+The blast file must contain the following fields, and have them as the header:
+```
+qseqid	sseqid	pident	length	mismatch	gapopen	qlen	qstart	qend	slen	sstart	send	evalue	bitscore
+```
+To get nice and usable output, run your blast search with, well here's a blastn example:
+```
+makeblastdb -in yourgenome.fasta -dbtype nucl -out yourgenome_db
+
+blastn -query yourtargets.fasta -db yourgenome_db -outfmt "6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore" -evalue 1e-6 -out blastn_yourtargets_to_yourgenome.txt  
+```
+Then just paste in the header line above before running `VetTargets_genome.R`.
+
+## `TargetSupercontigs.R` (forthcoming)
+Extract supercontigs from (draft) genome assemblies for more effective target capture sequencing in **closely related species**.
+
+### Rationale
+Phylogenomic projects in non-model organisms typically use exons as their targets, often even if their study group consists of very recently diverged, and therefore phylogenetically difficult, taxa. This results in significantly reduced coverage and poor assembly of intronic or flanking regions, which are consistently shown to improve phylogenetic inference due to their faster mutation rates. 
+
+At the same time, whole-genome shotgun sequencing is now relatively cheap, making draft genome assembly possible for us poor phylogeneticists working on non-model organisms. 
+
+`TargetSupercontigs.R` aims to locate target exons in the draft genome and extract the full exon + intron. These can then be used as targets instead of (or in combination with) the original exons, provided all the species you're working with are relatively closely related (at deep time scales introns will become too divergent to be useful as targets; exactly how deep the time scale limit is is very hard to say).
+
+## References
+<a id="1">(1)</a> Gatesy, J. & Springer, M. S. (2014). Phylogenetic analysis at deep timescales: Unreliable gene trees, bypassed hidden support, and the coalescence/concatalescence conundrum, _Molecular Phylogenetics and Evolution_, 80, 231-266.
+
+##### TODO:
+Make script to group exons from the same gene into separate target loci when introns are super big.
+##### DONE 
+Make sure to flag as paralogs regions on the same chromosome matching the same target.
---
+++ VetTargets_genome.R
@@ -0,0 +1,245 @@
+### This script will take a tabular blast result (-outfmt 6) with your target exons as the query and (draft) genome as the subject
+###  and do the following:
+###   1. Find potential paralogs by identifying genes with good matches from more than one contig/chromosome segments, or multiple positions within them, in the reference genome
+###   2. Find potentially missing genes by identifying genes with no or few good matches in the reference genome
+###   3. Find genes spanning huge introns
+###  then
+###   4. Create sets of simple segment plots to allow for visual inspection of blast alignments of genes failing and passing the above checks
+###   5. Output files listing names of targets passing or failing each check, and a file of 'clean' targets passing all checks
+
+### Arguments:
+###  --blast_file <tab-delimited blast result, target=query, genome=subject>
+###  --min_pident < % identity below which to discard blast matches prior to running checks >
+###  --max_intron_length < maximum allowable length of any given intron in a gene >
+
+###  --min_fragment_length < minimum length of a blast hit, ignore anything smaller >
+###  --output_prefix < prefix to name results files >
+
+## not yet implemented
+###  --max_intron_percent < maximum allowable percentage of a supercontig's length consisting of introns >
+
+GetCoverageStats<-function(data){
+  coverage_summary_chromosome_aware<-data.frame()
+  coverage_summary_chromosome_UNaware<-data.frame()
+  for(i in unique(data$qseqid)){
+    temp<-data %>% 
+      filter(qseqid==i)%>%
+      arrange(qstart) 
+   
+    # per-target+chromosome combo 
+    for(m in unique(temp$sseqid)){
+      temp2<-temp %>% filter(sseqid==m)
+      target_seq<-seq(1:unique(temp2$qlen))
+      for(f in 1:nrow(temp2)){
+        target_seq<-c(target_seq,seq(temp2[f,]$qstart,temp2[f,]$qend))
+      }
+      mycounts<-table(target_seq)-1 #subtract 1 because we've appended to the original sequence
+      paralog_percent<-length(mycounts[mycounts>1])/unique(temp2$qlen)
+      mean_paralogy_rate<-mean(mycounts)
+      full_percent<-length(mycounts[mycounts>0])/unique(temp2$qlen)
+      unique_percent<-length(mycounts[mycounts==1])/unique(temp2$qlen)
+      missing_percent<-length(mycounts[mycounts==0])/unique(temp2$qlen)
+      coverage_summary_chromosome_aware<-rbind(coverage_summary_chromosome_aware,
+                              data.frame(qseqid=unique(temp2$qseqid),
+                                         sseqid=unique(temp2$sseqid),
+                                         mean_paralogy_rate=round(mean_paralogy_rate,1),
+                                         paralog_percent=paralog_percent,
+                                         full_percent=full_percent,
+                                         unique_percent=unique_percent,
+                                         missing_percent=missing_percent))
+    }
+    
+    # per-target only (ignoring which chromosome(s) it matches to)
+    target_seq2<-seq(1:unique(temp$qlen))
+    for(f in 1:nrow(temp)){
+      target_seq2<-c(target_seq2,seq(temp[f,]$qstart,temp[f,]$qend))
+    }
+    mycounts<-table(target_seq2)-1 #subtract 1 because we've appended to the original sequence
+    paralog_percent<-length(mycounts[mycounts>1])/unique(temp$qlen)
+    # weighted_paralog_percent<-(length(mycounts[mycounts>1])*mean(mycounts[mycounts>1])/unique(temp$qlen))
+    mean_paralogy_rate<-mean(mycounts)
+    full_percent<-length(mycounts[mycounts>0])/unique(temp$qlen)
+    unique_percent<-length(mycounts[mycounts==1])/unique(temp$qlen)
+    missing_percent<-length(mycounts[mycounts==0])/unique(temp$qlen)
+    coverage_summary_chromosome_UNaware<-rbind(coverage_summary_chromosome_UNaware,
+                                             data.frame(qseqid=unique(temp$qseqid),
+                                                        mean_paralogy_rate=round(mean_paralogy_rate,1),
+                                                        paralog_percent=paralog_percent,
+                                                        full_percent=full_percent,
+                                                        unique_percent=unique_percent,
+                                                        missing_percent=missing_percent))
+  }
+  # round and convert to percentage
+  coverage_summary_chromosome_aware<-data.frame(cbind(coverage_summary_chromosome_aware[,1:3],apply(coverage_summary_chromosome_aware[,4:ncol(coverage_summary_chromosome_aware)],2,function(x) round(x,3)*100)))
+  coverage_summary_chromosome_UNaware<-data.frame(cbind(coverage_summary_chromosome_UNaware[,1:2],apply(coverage_summary_chromosome_UNaware[,3:ncol(coverage_summary_chromosome_UNaware)],2,function(x) round(x,3)*100)))
+  
+  coverage_summary_nchroms<-coverage_summary_chromosome_aware %>%
+    group_by(qseqid) %>%
+    summarise(n_chroms=n(),
+              .groups = "keep")
+  coverage_summary_chromosome_UNaware<-left_join(coverage_summary_chromosome_UNaware,coverage_summary_nchroms,"qseqid")
+  
+  return(list(coverage_summary_chromosome_aware=coverage_summary_chromosome_aware,coverage_summary_chromosome_unaware=coverage_summary_chromosome_UNaware))
+}
+
+FindIntrons<-function(data,max_intron_length,max_intron_percent){
+  introns_out<-data.frame()
+  introns_flag_out<-data.frame()
+  for(i in unique(data$qseqid)){
+    
+    temp<-data %>% 
+      filter(qseqid==i)
+    
+    introns <-  temp %>% group_by(qseqid,sseqid)%>% 
+      mutate(p_sstart=ifelse(sstart<send,sstart,send),
+             p_send=ifelse(p_sstart==sstart,send,sstart),
+             orientation=ifelse(p_sstart==sstart,"forward","reverse"))%>% 
+      arrange(p_sstart,.by_group = TRUE) %>%
+      summarise(shift_sstart=c(p_sstart,NA),shift_send=c(NA,p_send),
+                intron_length=shift_sstart-shift_send,
+                orientation=c(orientation,NA),
+                intron_position_on_target=c(qend,NA),
+                supercontig_length=max(p_send)-min(p_sstart),
+                .groups = "keep")%>%
+      na.exclude() %>%
+      select(qseqid,sseqid,intron_length,intron_position_on_target,supercontig_length)
+    ## write to data.frame
+    introns_out<-rbind(introns_out,introns)
+    
+    introns_flag<-introns %>%
+      summarise(longest_intron_length=max(intron_length),
+                total_intron_length=sum(intron_length),
+                largest_intron_percent=max(sum(intron_length)/unique(supercontig_length))*100,
+                exceeds_max_intron_length=ifelse(any(intron_length>=max_intron_length),TRUE,FALSE),
+                exceeds_max_intron_percent=ifelse(largest_intron_percent>=max_intron_percent,TRUE,FALSE),
+                summed_supercontig_length=unique(supercontig_length),
+                .groups="keep") %>%
+      ungroup() %>%
+      select(qseqid,sseqid,summed_supercontig_length,
+             longest_intron_length,total_intron_length,exceeds_max_intron_length,
+             largest_intron_percent,exceeds_max_intron_percent) %>%
+      unique()
+    
+    introns_flag_out<-rbind(introns_flag_out,introns_flag)
+  }
+  return(list(intron_details=introns_out,targets_with_intron_flags=introns_flag_out))
+}
+
+PlotTargets<-function(data,output_prefix,min_display_intron,max_display_intron){
+  pdf(paste0(output_prefix,"_plots.pdf"),width=10)
+  for(i in unique(data$qseqid)){
+    
+    temp<-data %>% 
+      filter(qseqid==i)
+    
+    introns <-  temp %>% group_by(qseqid,sseqid)%>% 
+      mutate(p_sstart=ifelse(sstart<send,sstart,send),
+             p_send=ifelse(p_sstart==sstart,send,sstart),
+             orientation=ifelse(p_sstart==sstart,"forward","reverse"))%>% 
+      arrange(p_sstart,.by_group = TRUE) %>%
+      summarise(shift_sstart=c(p_sstart,NA),shift_send=c(NA,p_send),
+                intron=(shift_sstart-shift_send),
+                qpos=c(NA,qend),shift_qstart=c(qstart,NA),
+                orientation=c(orientation,NA),
+                .groups = "keep")%>%
+      filter(intron>min_display_intron,intron<max_display_intron)%>%
+      na.exclude()
+    
+    p<-ggplot(temp)+
+      geom_segment(aes(y=sstart,yend=send,x=qstart,xend=qend,colour=bitscore),
+                   show.legend = T,size=2.5)+
+      scale_colour_viridis_c("Sequence\nsimilarity")+
+      facet_wrap(~sseqid,scales = "free")+
+      xlim(c(0,unique(temp$qlen)))+
+      theme_bw()+
+      labs(x="Target position",y="Genome position")+
+      ggtitle(i)+
+      coord_flip()
+    if(nrow(introns)>0){
+      p<-p+geom_rect(data=introns,
+                     aes(xmin=-Inf,xmax=Inf,ymin=shift_sstart,ymax=shift_send),
+                     colour="darkgreen",alpha=0.05,size=NA)+
+        geom_label_repel(data=introns,
+                         aes(x=qpos,
+                             y=shift_sstart-(intron/2),
+                             label=intron),fill="white",
+                         force_pull = 0.1,
+                         size=2,
+                         min.segment.length=unit(5,"mm"),
+                         box.padding = 0.05,label.padding=0.07,
+                         label.r=0.01,seed=1234,
+                         max.overlaps=15)
+    }
+    
+    
+    print(p)
+  }
+  dev.off()
+  
+}
+
+CheckTargets<-function(blast_file,
+                       min_pident,min_fragment_length,
+                       max_intron_length,max_intron_percent,
+                       output_prefix,
+                       min_display_intron,max_display_intron){
+  dat<-as_tibble(read.table(blast_file,header=T))
+  dat<-dat %>%
+    filter(pident >= min_pident,
+           length >= min_fragment_length)
+  
+  cov_stats<-GetCoverageStats(dat)
+  write.table(cov_stats$coverage_summary_chromosome_aware,paste0(output_prefix,"_CoverageStats_PerChromosome.txt"),quote = F,row.names = F,col.names = TRUE)
+  write.table(cov_stats$coverage_summary_chromosome_unaware,paste0(output_prefix,"_CoverageStats_AcrossChromosomes.txt"),quote = F,row.names = F,col.names = TRUE)
+  
+  intron_stats<-FindIntrons(data=dat,max_intron_length,max_intron_percent)
+  write.table(intron_stats$intron_details,paste0(output_prefix,"_IntronStats.txt"),quote = F,row.names = F,col.names = TRUE)
+  write.table(intron_stats$targets_with_intron_flags,paste0(output_prefix,"_IntronFlags.txt"),quote = F,row.names = F,col.names = TRUE)
+  
+  PlotTargets(dat,output_prefix,min_display_intron,max_display_intron)
+  
+}
+
+#### DONE DEFINING FUNCTIONS
+suppressMessages(suppressWarnings(require(optparse)))
+
+p <- OptionParser(usage=" This script will take a tabular blast result (-outfmt 6) with your target exons as the query and (draft) genome as the subject\n
+   ***WITH A HEADER LINE**\n
+  and do the following:\n
+   1. Find potential paralogs by identifying genes with good matches from more than one contig/chromosome segments in the reference genome\n
+   2. Find potentially missing genes by identifying genes with no or few good matches in the reference genome\n
+   3. Find genes spanning huge introns\n
+  then\n
+   4. Create sets of simple segment plots to allow for visual inspection of blast alignments of genes failing and passing the above checks\n
+   5. Output files listing names of targets passing or failing each check, and a file of 'clean' targets passing all checks\n
+  Run using Rscript, e.g.\n
+  Rscript VetTargets_genome.R --blast_file blastn_targets_to_genome.txt --min_fragment_length 50 --min_pident 80 --max_intron_length 10000 --max_intron_percent 60 --output_prefix test")
+# Add a positional argument
+p <- add_option(p, c("--blast_file"), help="<Required: tab-delimited blast result, target=query, genome=subject>",type="character")
+p <- add_option(p, c("--min_fragment_length"), help="<Required: minimum length of a blast hit, ignore anything shorter>",type="numeric")
+p <- add_option(p, c("--min_pident"), help="<Required: minimum % identity of a blast hit, ignore anything lower>",type="numeric")
+p <- add_option(p, c("--max_intron_length"), help="<Required: length threshold of the largest intron in a gene; flag any gene exceeding>",type="numeric")
+p <- add_option(p, c("--max_intron_percent"), help="<Required: percentage of a supercontig's length consisting of introns; flag any gene exceeding>",type="numeric")
+p <- add_option(p, c("--output_prefix"), help="<prefix to name results files; defaults to CheckTargets_results>",type="character",default = "CheckTargets_results")
+p <- add_option(p, c("--min_display_intron"), help="<intron length above which to annotate on plots; default 1kb>",type="numeric",default=1000)
+p <- add_option(p, c("--max_display_intron"), help="<don't annotate introns longer than this; default 1Mb>",type="numeric",default=1e6)
+
+# parse
+args<-parse_args(p)
+
+suppressMessages(suppressWarnings(require(tidyverse,quietly=TRUE,warn.conflicts=FALSE)))
+suppressMessages(suppressWarnings(require(ggrepel,quietly=TRUE,warn.conflicts=FALSE)))
+
+## RUN
+CheckTargets(blast_file=args$blast_file,
+             min_pident=args$min_pident,
+             min_fragment_length=args$min_fragment_length,
+             max_intron_length=args$max_intron_length,
+             max_intron_percent=args$max_intron_percent,
+             output_prefix=args$output_prefix,
+             min_display_intron=args$min_display_intron,
+             max_display_intron=args$max_display_intron)
+
+
+
+             
