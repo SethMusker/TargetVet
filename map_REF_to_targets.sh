27214e9937bcf18c03d112ffbe46b7e158f04384
Seth Musker <sethmusker@gmail.com>
2021-10-11 14:14:03 +0200

    Update map_REF_to_targets.sh

--- map_REF_to_targets.sh
+++ map_REF_to_targets.sh
@@ -40,7 +40,7 @@ done
 REF_BASE=`basename ${REF}`
 db_exist_count=`ls ${REF_BASE}_db.* | wc -l`
 
-if [[ ${db_exist_count} == 6 ]]; then
+if [[ ${db_exist_count} == 7 ]]; then
     echo "Assuming blast database exists."
 else
     echo "Making blast database..."
@@ -48,25 +48,29 @@ else
 fi
 
 if [[ ${BLAST_TYPE} == "tblastx" ]]; then
-      echo "Using tblastx."
-      tblastx -query ${TARGET} \
+     echo "Using tblastx."
+     tblastx -query ${TARGET} \
         -db ${REF_BASE}_db \
         -out `basename ${TARGET}`_to_${REF_BASE}.tblastx.txt.temp \
         -evalue ${evalue} \
         -num_threads ${threads} \
         -outfmt "6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore"
-   echo -e "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqlen\tqstart\tqend\tslen\tsstart\tsend\tevalue\tbitscore" | \
+     echo -e "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqlen\tqstart\tqend\tslen\tsstart\tsend\tevalue\tbitscore" | \
         cat - `basename ${TARGET}`_to_${REF_BASE}.tblastx.txt.temp > `basename ${TARGET}`_to_${REF_BASE}.tblastx.txt
+     rm `basename ${TARGET}`_to_${REF_BASE}.tblastx.txt.temp
    else
-      echo "Using blastn."
-      blastn -query ${TARGET} \
+     echo "Using blastn."
+     blastn -query ${TARGET} \
         -db ${REF_BASE}_db \
         -out `basename ${TARGET}`_to_${REF_BASE}.blastn.txt.temp \
         -evalue ${evalue} \
         -num_threads ${threads} \
         -outfmt "6 qseqid sseqid pident length mismatch gapopen qlen qstart qend slen sstart send evalue bitscore"      
-   echo -e "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqlen\tqstart\tqend\tslen\tsstart\tsend\tevalue\tbitscore" | \
+     echo -e "qseqid\tsseqid\tpident\tlength\tmismatch\tgapopen\tqlen\tqstart\tqend\tslen\tsstart\tsend\tevalue\tbitscore" | \
         cat - `basename ${TARGET}`_to_${REF_BASE}.blastn.txt.temp > `basename ${TARGET}`_to_${REF_BASE}.blastn.txt
+     rm `basename ${TARGET}`_to_${REF_BASE}.blastn.txt.temp
    fi
+
+
 
 echo "Done!"
